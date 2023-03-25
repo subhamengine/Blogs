e2466b4e0271069724741f0ab0cd8a3363343dfe
subham sharma <subhamengine@gmail.com>
2023-03-25 16:06:53 +0530

    initial

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const Header = () => {
-    return (
-        <div>Header</div>
-    )
-}
+const Header = ({ title }) => {
+  return <header className="Header">{title}</header>;
+};
 
-export default Header
+export default Header;
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Feed from "./Feed";
+const Home = ({ posts }) => {
+  console.log(posts);
+  return (
+    <main className="Home">
+      {posts.length ? (
+        <Feed posts={posts} />
+      ) : (
+        <p style={{ marginTop: "2rem" }}>No Posts to display.</p>
+      )}
+    </main>
+  );
+};
 
-const Home = () => {
-    return (
-        <div>Home</div>
-    )
-}
-
-export default Home
+export default Home;
