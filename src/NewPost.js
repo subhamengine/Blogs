3df9bf114b7e8f1c92b4118a82a6bb1ee62ea50a
subham sharma <subhamengine@gmail.com>
2023-03-26 12:12:16 +0530

    create_post

--- src/NewPost.js
+++ src/NewPost.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
-const NewPost = () => {
-    return (
-        <div>NewPost</div>
-    )
-}
+const NewPost = ({
+  handleSubmit,
+  postTitle,
+  setPostTitle,
+  postBody,
+  setPostBody,
+}) => {
+  return (
+    <main className="NewPost">
+      <h2>New Post</h2>
+      <form className="newPostForm" onSubmit={handleSubmit}>
+        <label htmlFor="postTitle">Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          required
+          value={postTitle}
+          onChange={(e) => setPostTitle(e.target.value)}
+        />
+        <label htmlFor="postBody">Post:</label>
+        <textarea
+          id="postBody"
+          value={postBody}
+          onChange={(e) => setPostBody(e.target.value)}
+          required
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
+};
 
-export default NewPost
+export default NewPost;
